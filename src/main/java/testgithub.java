a0a1010dc05bea8a10460475be20d3dc9f3f1cbf
mturanCT <95585767+mturanCT@users.noreply.github.com>
2022-04-23 20:48:41 -0400

    Update testgithub.java

--- src/main/java/testgithub.java
+++ src/main/java/testgithub.java
@@ -1,5 +1,7 @@
 public class testgithub {
     public static void main(String[] args) {
         System.out.println("this is a test1 for github");
+        
+        System.out.println("this is a test2 from github");
     }
 }
